b67a4a7ab8050b742eff9f8455ccdf9f77d6b50e
Mohammad Al-Husseini <31898931+menarus@users.noreply.github.com>
2017-09-12 19:23:17 -0400

    Update 2-11 Distance per Tank of Gas.cpp

--- Solutions/Ch2/2-11 Distance per Tank of Gas.cpp
+++ Solutions/Ch2/2-11 Distance per Tank of Gas.cpp
@@ -3,8 +3,8 @@ Project			: Distance per Tank of Gas
 Author			: Mohammad Al-Husseini
 Description		: Calculates the distance a car can travel on a single tank of gas over town and highway.
 Known			: 20 Gallon Tank
-			  21.5 MPG Town
-			  26.8 MPG Highway
+				  21.5 MPG Town
+				  26.8 MPG Highway
 Display			: The number of miles in town and highway.
 */
 ///////////////////////////////////////////////////////////////////////////////////////////////////
