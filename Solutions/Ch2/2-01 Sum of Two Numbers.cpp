2451e2d271825e746fdafe0f30e05d567fe40ed2
Mohammad Al-Husseini <31898931+menarus@users.noreply.github.com>
2017-09-12 19:21:30 -0400

    Update 2-01 Sum of Two Numbers.cpp

--- Solutions/Ch2/2-01 Sum of Two Numbers.cpp
+++ Solutions/Ch2/2-01 Sum of Two Numbers.cpp
@@ -1,8 +1,8 @@
 /*
-Project	    : Sum of Two Numbers
-Author      : Mohammad Al-Husseini
-Description : Stores the integers 62 and 99 in variables.
-              Then store the result of their addition into a variable named total.
+Project			: Sum of Two Numbers
+Author			: Mohammad Al-Husseini
+Description		: Stores the integers 62 and 99 in variables.
+				  Then store the result of their addition into a variable named total.
 */
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -11,14 +11,14 @@ using namespace std;
 
 int main()
 {
-    // Store our known values
-    int num1 = 62;
-    int num2 = 99;
+	// Store our known values
+	int num1 = 62;
+	int num2 = 99;
 
-    // Calculate the result
-    int total = num1 + num2;
+	// Calculate the result
+	int total = num1 + num2;
 
-    // Display the result
-    cout << "The sum of " << num1 << " and " << num2 << " is " << total << ".";
-    return 0;
+	// Display the result
+	cout << "The sum of " << num1 << " and " << num2 << " is " << total << ".";
+	return 0;
 }
