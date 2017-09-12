2277b81d99126558a846ffe31dc1a85307779131
Mohammad Al-Husseini <31898931+menarus@users.noreply.github.com>
2017-09-12 19:22:40 -0400

    Update 2-10 Miles Per Gallon.cpp

--- Solutions/Ch2/2-10 Miles Per Gallon.cpp
+++ Solutions/Ch2/2-10 Miles Per Gallon.cpp
@@ -3,7 +3,7 @@ Project			: Miles Per Gallon
 Author			: Mohammad Al-Husseini
 Description		: Calculates MPG.
 Known			: 12 Gallon Tank
-			  350 Miles before refeuling
+				  350 Miles before refeuling
 Display			: The MPG.
 */
 ///////////////////////////////////////////////////////////////////////////////////////////////////
